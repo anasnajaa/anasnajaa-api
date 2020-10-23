913cfe9aaa2f3c0809947970c1ecb81130d9200b
A.N <spidernet12@gmail.com>
2020-10-23 18:09:22 +0300

    Update sms.r.js

--- routes/sms.r.js
+++ routes/sms.r.js
@@ -2,6 +2,4 @@ const smsController = require('../controllers/sms.c');
 
 exports.init = (router)=>{
     router.post('/sms/update-sms-status', smsController.updateMessageStatus);
-    router.post('/sms/send', smsController.sendMessage);
-    router.get('/sms/messages', smsController.getMessages);
 }
