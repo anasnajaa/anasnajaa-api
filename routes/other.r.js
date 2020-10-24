a9df543eae00f5e6bc7363b5dbd8c9083ab0d323
A.N <spidernet12@gmail.com>
2020-10-24 14:26:23 +0300

    added cpatcha validation endpoint for testing purposes

--- routes/other.r.js
+++ routes/other.r.js
@@ -1,7 +1,16 @@
 const {v4} = require('uuid');
+const {isCaptchaValid} = require('../util/cpatch');
 
 exports.init = (router)=>{
     router.get('/awake', async (req, res, next)=>{
         res.json({id: v4()});
+    });
+
+    router.get("/captcha", async(req, res, next) => {
+        const key = req.query.key;
+        const captchaResult = await isCaptchaValid(key);
+        res.json({
+            isValid: captchaResult
+        })
     });
 }
