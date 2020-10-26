74846785c73f789e948bd42d47f37db387b276ab
A.N <spidernet12@gmail.com>
2020-10-26 17:34:34 +0300

    added localized strings

--- locales/codedResponses.js
+++ locales/codedResponses.js
@@ -1,5 +1,6 @@
 const m = (c, m)=>{ return { code: c, message: m }; };
 
 module.exports = { 
-    test:                                 t => m(0, t('test'))
+    test:                                   t => m(0, t('test')),
+    verification_code_sent:                 t => m(0, t('verification_code_sent')),
 };
