af0c24380d1498fe0349ef14e13f3165c5e2023c
A.N <spidernet12@gmail.com>
2020-10-26 17:35:52 +0300

    added captcha validation and mobile validation

--- validators/index.js
+++ validators/index.js
@@ -1,4 +1,12 @@
 const validator = require('validator');
+const { isCaptchaValid } = require('../util/captcha');
+
+exports.vCaptcha = async(fieldName, errors, captcha) => {
+    const captchaResult = await isCaptchaValid(captcha);
+    if(!captchaResult) {
+        errors[fieldName] = "Captcha is not valid, please try again";
+    }
+}
 
 exports.vEmail = (fieldName, errors, email)=> {
     if(!validator.isEmail(email)) {
@@ -19,6 +27,16 @@ exports.vNumeric = (fieldName, errors, value)=> {
         }
     } else {
         errors[fieldName] = "Must be a number";
+    }
+};
+
+exports.vMobile = (fieldName, errors, value)=> {
+    if(value !== undefined && value !== null) {
+        if(!validator.isMobilePhone(value.toString(), ['ar-KW'])){
+            errors[fieldName] = "Must be a valid Kuwait mobile number";
+        }
+    } else {
+        errors[fieldName] = "Must be a valid Kuwait mobile number";
     }
 };
 
