876ff9f36d6a103e84ed738ef18a56c4b0448204
A.N <spidernet12@gmail.com>
2020-10-27 15:06:25 +0300

    added transaction to verifyAuthCode and finalized the intial verification logic

--- controllers/serviceRequest.c.js
+++ controllers/serviceRequest.c.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const environment = process.env.NODE_ENV;
 const { isEmpty } = require('lodash');
 const { 
     vEmail, 
@@ -9,7 +11,9 @@ const {
 const { apiError } = require('../util/errorHandler');
 const smsService = require('./sms.c');
 const UserModel = require('../models/user.m');
+const UserServiceModel = require('../models/userService.m');
 const r = require('../locales/codedResponses');
+const mongoose = require('mongoose');
 
 exports.sendVerificationCode = async (req, res, next) => {
     const t = req.__;
@@ -27,12 +31,14 @@ exports.sendVerificationCode = async (req, res, next) => {
         const generateAuthCode = () => Math.floor(Math.random()*90000) + 10000;
 
         const sendAuthSMS = async (mobileWithCountryCode, verificationCode) => {
-            return await smsService.sendMessage(
-                "verification",
-                mobileWithCountryCode, 
-                `${t("your_verification_code")}: ${verificationCode}`);
+            if(environment === "production"){
+                return await smsService.sendMessage(
+                    "verification",
+                    mobileWithCountryCode, 
+                    `${t("your_verification_code")}: ${verificationCode}`);
+            }
+            return null;
         }
-
 
 
 
@@ -50,8 +56,7 @@ exports.sendVerificationCode = async (req, res, next) => {
         // reset verification as we do not have a login system yet 
         const updatedUser = await UserModel.updateOne({mobile: mobileWithCountryCode}, {
             isMobileVerified: false,
-            verificationDate: null,
-            verificationCode: authCode
+            mobileAuthCode: authCode
         }).lean();
 
         if(updatedUser.nModified > 0) {
@@ -65,9 +70,8 @@ exports.sendVerificationCode = async (req, res, next) => {
 
         const newUser = await UserModel.create({
             mobile: mobileWithCountryCode, 
-            verificationCode: authCode,
             isMobileVerified: false,
-            verificationDate: null
+            mobileAuthCode: authCode
         });
 
         if(newUser){
@@ -87,25 +91,75 @@ exports.sendVerificationCode = async (req, res, next) => {
     }
 };
 
-exports.verifyMobile = async (req, res, next) => {
+
+/* 
+this function will:
+- update user verification details 
+- add a new user service record
+- return added user service id as a reference 
+*/
+exports.verifyAuthCode = async (req, res, next) => {
+    const session = await mongoose.startSession();
     try {
         let errors = {};
         const { 
-            serviceRequestId,
-            mobileVerificationCode
+            authCode,
+            mobile,
+            countryCode
         } = req.body;
 
+        const mobileWithCountryCode = countryCode + mobile;
 
+        vEmpty("authCode", errors, authCode);
+        vMobile("mobile", errors, mobileWithCountryCode);
 
         if(!isEmpty(errors)){
             res.json({status: -1, errors});
             return;
         }
 
-        res.json({status: 1});
+        session.startTransaction();
+        
+        const user = await UserModel.findOne({
+            mobileAuthCode: authCode,
+            mobile: mobileWithCountryCode
+        }).session(session);
 
+        if(!user){
+            await session.abortTransaction();
+            res.json({status: -1, error: "Auth code or mobile number is not valid, please restart the verification process."});
+            return;
+        }
+
+        user.isMobileVerified = true;
+        user.mobileAuthCode = null;
+        
+        const userService = await UserServiceModel.create([{
+            userId: user._id
+        }], {session});
+
+        const newServiceId = userService[0]._id;
+
+        user.services.push(newServiceId);
+
+        const updatedUser = await user.save();
+
+        if(!updatedUser) {
+            await session.abortTransaction();
+            res.json({status: -1, error: "Failed to update user details."});
+            return;
+        }
+
+        await session.commitTransaction();
+
+        res.json({
+            status: 1,
+            message: "Mobile verified",
+            userServiceId: newServiceId
+        });
     } catch (error) {
-        apiError(error);
+        await session.abortTransaction();
+        apiError(res, error);
     }
 }
 
@@ -115,7 +169,7 @@ exports.addInfo = async (req, res, next)=>{
         const { 
             name, 
             email, 
-            serviceId, 
+            userServiceId, 
             description 
         } = req.body;
 
@@ -132,6 +186,6 @@ exports.addInfo = async (req, res, next)=>{
         const result = await smsModel.updateOne({ sid: sms.SmsSid }, sms).exec();
         res.json({status: 1, result});
     } catch (error) {
-        apiError(error);
+        apiError(res, error);
     }
 };
