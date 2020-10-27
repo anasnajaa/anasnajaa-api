38ca159c6a22383256d476d87aa6d85533ee6bd9
A.N <spidernet12@gmail.com>
2020-10-27 15:04:32 +0300

    added email verification keys and auth code keys

--- models/user.m.js
+++ models/user.m.js
@@ -6,8 +6,9 @@ const schema = new Schema({
     email: { type: String, required: false },
     mobile: { type: String, required: true, unique: true },
     isMobileVerified: { type: Boolean, required: false },
-    verificationDate: { type: Boolean, required: false },
-    verificationCode: { type: String, required: false },
+    isEmailVerified: { type: Boolean, required: false },
+    mobileAuthCode: { type: String, required: false },
+    emailAuthCode: { type: String, required: false },
     services: [{
         type: Schema.Types.ObjectId,
         ref: 'userService'
