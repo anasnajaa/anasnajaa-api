27cf6e6b55c675e4d6ed10b5040354c13aba743b
A.N <spidernet12@gmail.com>
2020-10-27 15:03:44 +0300

    removed time stamps and specified collection name

--- models/service.m.js
+++ models/service.m.js
@@ -5,6 +5,8 @@ const schema = new Schema({
     name: { type: String, required: false },
     description: { type: String, required: false }
 }, 
-{ timestamps: true });
+{ timestamps: false });
+
+schema.index({name: 1, collection: 'userService'});
 
 module.exports = mongoose.model('service', schema);
