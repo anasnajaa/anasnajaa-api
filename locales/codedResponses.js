9f7aa430af40ac6d13bccc239e78bf7fcd767188
A.N <spidernet12@gmail.com>
2020-10-27 12:23:38 +0300

    added new strings

--- locales/codedResponses.js
+++ locales/codedResponses.js
@@ -1,6 +1,7 @@
 const m = (c, m)=>{ return { code: c, message: m }; };
 
 module.exports = { 
-    test:                                   t => m(0, t('test')),
-    verification_code_sent:                 t => m(0, t('verification_code_sent')),
+    test:                                           t => m(0, t('test')),
+    verification_code_mobile_sent:                  t => m(1, t('verification_code_mobile_sent')),
+    verification_code_email_sent:                   t => m(2, t('verification_code_email_sent')),
 };
