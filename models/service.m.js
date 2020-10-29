e992b7f8939d183d8b459e88081771e187a43166
A.N <spidernet12@gmail.com>
2020-10-29 21:53:52 +0300

    moved collection name into the main schema object

--- models/service.m.js
+++ models/service.m.js
@@ -3,10 +3,9 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     name: { type: String, required: false },
-    description: { type: String, required: false }
+    description: { type: String, required: false },
+    index: { type: Number, required: false }
 }, 
-{ timestamps: false });
-
-schema.index({name: 1, collection: 'userService'});
+{ timestamps: false, collection: 'service' });
 
 module.exports = mongoose.model('service', schema);
