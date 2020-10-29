530e3d31c6abe2abc98f970dcc8110718176aeff
A.N <spidernet12@gmail.com>
2020-10-29 21:57:38 +0300

    refactored serviceRequest.c to seperate controllers

--- routes/serviceRequest.r.js
+++ routes/serviceRequest.r.js
@@ -1,7 +1,10 @@
-const cServiceRequests = require('../controllers/serviceRequest.c');
-
 exports.init = (router) => {
-    router.post("/request/service/send-verification-code", cServiceRequests.sendVerificationCode);
-    router.post("/request/service/verify", cServiceRequests.verifyAuthCode);
-    router.post("/request/service/add-info", cServiceRequests.addInfo);
+    router.post("/request/service/send-verification-code", 
+    require('../controllers/serviceRequest/sendVerificationCode.c'));
+
+    router.post("/request/service/verify", 
+    require('../controllers/serviceRequest/verifyAuthCode.c'));
+
+    router.post("/request/service/add-info", 
+    require('../controllers/serviceRequest/addUserInfo.c'));
 }
